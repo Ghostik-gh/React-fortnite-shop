249efbbf6b2e038ea510c26d121ac83b6807093d
Ghostik-gh <feodor200@mail.ru>
2022-08-07 10:04:43 +0300

    add shop & config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Footer } from "./Footer";
+import { Header } from "./Header";
+import { Shop } from "./Shop";
 
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Header />
+      <Shop />
+      <Footer />
+    </>
   );
 }
 
